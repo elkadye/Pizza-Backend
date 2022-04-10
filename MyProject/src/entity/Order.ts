55c508fda8aa13baf559c51e3a4a9e8087f91f1c
elkadye <73303777+elkadye@users.noreply.github.com>
2022-04-10 14:32:18 +0200

    incl user info in order table + rmv user table

--- MyProject/src/entity/Order.ts
+++ MyProject/src/entity/Order.ts
@@ -1,19 +1,30 @@
-import { Entity, ManyToOne,OneToMany, ManyToMany, JoinTable } from "typeorm";
+import { Entity, ManyToOne, OneToMany, Column, JoinTable } from "typeorm";
 import { EmBase } from "./EmBase";
-import { User } from "./User";
+// import { User } from "./User";
 
 import { OrderProduct } from "./OrderProduct";
 
 @Entity()
 export class Order extends EmBase {
-  
-  @ManyToOne(() => User, (user) => user.orders, { nullable: false })
-  user: User;
+  @Column({ nullable: false })
+  name: string;
 
-  //   @ManyToMany(() => Product, (product)=> product.orders)
-  //   @JoinTable()
-  //   products: Product[];
+  @Column({ nullable: false })
+  mobile: number;
+
+  @Column()
+  address: string;
+
+  @Column()
+  city: string;
 
   @OneToMany(() => OrderProduct, (orderProduct) => orderProduct.order)
   public orderProducts!: OrderProduct[];
-}
+};
+
+// @ManyToOne(() => User, (user) => user.orders, { nullable: false })
+// user: User;
+
+//   @ManyToMany(() => Product, (product)=> product.orders)
+//   @JoinTable()
+//   products: Product[];
